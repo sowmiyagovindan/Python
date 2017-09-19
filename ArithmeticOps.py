7e6dff818640b3863b5930d4776bb0c5ba3cbeea
Sowmiya Govindan Kutty <sowmiyag@nvidia.com>
2017-09-19 13:54:16 -0700

    included division ops

--- ArithmeticOps.py
+++ ArithmeticOps.py
@@ -1,17 +1,23 @@
+from decimal import Decimal
+
 class ArithmeticOps():
     def add(self,a,b):
-        ops = a+b;
+        ops = a+b
         print(ops)
 
     def subtract(self,a,b):
-        ops = a-b;
+        ops = a-b
         print(ops)
 
     def multiply(self, a, b):
-        ops = a*b;
-        print(ops)
+        ops = a*b
+        print(ops, format(ops,'.2f'))
 
+    def divide(self, a, b):
+        ops = a/b
+        print(ops)
 optn = ArithmeticOps()
-optn.add(2,3);
+optn.add(2,3)
 optn.subtract(2,3)
 optn.multiply(10,100)
+optn.divide(1000,100)
