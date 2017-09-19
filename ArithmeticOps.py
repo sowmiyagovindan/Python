89027f06406dc27c41f680c1b38aeed4597d1ede
Sowmiya Govindan Kutty <sowmiyag@nvidia.com>
2017-09-19 11:59:35 -0700

    corrected multiplication function

--- ArithmeticOps.py
+++ ArithmeticOps.py
@@ -8,10 +8,10 @@ class ArithmeticOps():
         print(ops)
 
     def multiply(self, a, b):
-        ops = a - b;
+        ops = a*b;
         print(ops)
 
 optn = ArithmeticOps()
 optn.add(2,3);
 optn.subtract(2,3)
-optn.multiply(10*100)
+optn.multiply(10,100)
