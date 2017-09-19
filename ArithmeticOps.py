32999cea0f63aba5abaa4b9e1aa7d6e601b0ac86
Sowmiya Govindan Kutty <sowmiyag@nvidia.com>
2017-09-19 11:47:42 -0700

    Arithmetic operations

---
+++ ArithmeticOps.py
@@ -0,0 +1,17 @@
+class ArithmeticOps():
+    def add(self,a,b):
+        ops = a+b;
+        print(ops)
+
+    def subtract(self,a,b):
+        ops = a-b;
+        print(ops)
+
+    # def multiply(self, a, b):
+    #     ops = a - b;
+    #     print(ops)
+
+optn = ArithmeticOps()
+optn.add(2,3);
+optn.subtract(2,3)
+# optn.multiply(10*100)
