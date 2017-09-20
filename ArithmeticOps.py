c40dd78cc868c5c396748e300cc15ec8a1e385f2
Sowmiya Govindan Kutty <sowmiyag@nvidia.com>
2017-09-20 13:40:37 -0700

    changed values

--- ArithmeticOps.py
+++ ArithmeticOps.py
@@ -11,13 +11,14 @@ class ArithmeticOps():
 
     def multiply(self, a, b):
         ops = a*b
-        print(ops, format(ops,'.2f'))
+        print(format(ops,'.2f'))
 
     def divide(self, a, b):
         ops = a/b
         print(ops)
+
 optn = ArithmeticOps()
-optn.add(2,3)
+optn.add(10,3)
 optn.subtract(2,3)
 optn.multiply(10,100)
 optn.divide(1000,100)
